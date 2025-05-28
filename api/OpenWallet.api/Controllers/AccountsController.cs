d3d435d972f0bab601a30ff9d213e91c1ec3e518
Henrique Araújo Neto <anhenrique93@gmail.com>
2025-05-28 13:04:21 +0100

    Change AccountsController to use MapToAccount extension Method

--- api/OpenWallet.api/Controllers/AccountsController.cs
+++ api/OpenWallet.api/Controllers/AccountsController.cs
@@ -18,17 +18,7 @@ namespace OpenWallet.Api.Controllers
         [HttpPost("accounts")]
         public async Task<IActionResult> Create([FromBody] CreateAccountRequest request)
         {
-            var account = new OpenWallet.Application.Models.Account
-            {
-                Id = Guid.NewGuid(),
-                Name = request.Name,
-                Description = request.Description,
-                Currency = request.Currency,
-                Type = request.Type,
-                Balance = request.InitialBalance,
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
-            };
+            var account = request.MapToAccount();
 
             var result = await _accountRepository.CreateAsync(account);
             return Created($"/api/accounts/{account.Id}", account);
