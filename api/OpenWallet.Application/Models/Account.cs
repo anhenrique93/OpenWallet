3f22a3a173b4140a7c9dba124aa3024e56d6083b
Henrique Araújo Neto <anhenrique93@gmail.com>
2025-05-28 13:02:44 +0100

    Refactor Account: protect constructor, add factory method, and expose update methods.

--- api/OpenWallet.Application/Models/Account.cs
+++ api/OpenWallet.Application/Models/Account.cs
@@ -8,13 +8,54 @@ namespace OpenWallet.Application.Models
 {
     public class Account
     {
-        public required Guid Id { get; init; }
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public required string Currency { get; set; }
-        public required string Type { get; set; } // e.g., "Checking", "Savings", "Credit Card" TODO: Create a AccountCategory class
-        public required decimal Balance { get; set; }
-        public required DateTime CreatedAt { get; init; }
-        public required DateTime UpdatedAt { get; set; } 
+
+        protected Account() { } // EF Core
+
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Currency { get; private set; }
+        public string Type { get; private set; } // e.g., "Checking", "Savings", "Credit Card" TODO: Create a AccountCategory class
+        public decimal Balance { get; private set; }
+        public DateTime CreatedAt { get; init; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public static Account CreateNewAccount(string name, string description, string currency, string type, decimal? balance)
+        {
+            return new Account
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Description = description,
+                Currency = currency,
+                Type = type,
+                Balance = balance ?? 0m, // Default to 0 if no initial balance is provided
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateDescription(string description)
+        {
+            Description = description;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateType(string type)
+        {
+            Type = type;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void UpdateBalance(decimal value)
+        {
+            Balance += value;
+            UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
